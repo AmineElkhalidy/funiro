51f5d7e062948b92f0619048962f0d0ba8c508bb
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-11-16 22:25:39 +0100

    Adding data and hero section file...

---
+++ client/components/Home/Hero.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+// Splide & SplideSlide
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+
+// Data
+import { heroData } from "../../data/data";
+
+const Hero = () => {
+  return (
+    <React.Fragment>
+      {/* Hero section */}
+      <section className="" id="hero"></section>
+    </React.Fragment>
+  );
+};
+
+export default Hero;
---
+++ client/data/data.js
@@ -0,0 +1,27 @@
+import HeroImg1 from "../public/images/home-1.png";
+import HeroImg2 from "../public/images/home-2.png";
+import HeroImg3 from "../public/images/home-3.png";
+
+export const heroData = [
+  {
+    id: "slide-01",
+    image: HeroImg1,
+    name: "Bohauss",
+    description: "Luxury big sofa 2-seat",
+    surface: "Rp 17.000.000",
+  },
+  {
+    id: "slide-02",
+    image: HeroImg2,
+    name: "Bohauss",
+    description: "Luxury big sofa 2-seat",
+    surface: "Rp 17.000.000",
+  },
+  {
+    id: "slide-03",
+    image: HeroImg3,
+    name: "Bohauss",
+    description: "Luxury big sofa 2-seat",
+    surface: "Rp 17.000.000",
+  },
+];
