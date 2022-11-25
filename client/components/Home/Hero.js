594fdd7cb63b28e80c8206e079b7c390b9e6626d
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-11-25 21:09:23 +0100

    Fixing the footer...

--- client/components/Home/Hero.js
+++ client/components/Home/Hero.js
@@ -41,7 +41,7 @@ const Hero = () => {
       >
         <div className="relative pt-24">
           {/* Info box */}
-          <div className="absolute top-0 z-40 px-8 py-12 bg-white/80 backdrop-blur-sm background left-14">
+          <div className="absolute z-40 px-8 py-12 top-4 bg-white/80 backdrop-blur-sm background left-14">
             <h2 className="mb-8 text-4xl font-bold text-darkGray">
               High-Quality
               <br />
@@ -61,90 +61,75 @@ const Hero = () => {
               Shop Now
             </button>
           </div>
-
-          {/* Slider */}
-          <Swiper
-            className="relative"
-            modules={[Navigation, Pagination, Scrollbar, A11y]}
-            navigation
-            pagination={{ clickable: true }}
-            slidesPerView={1.85}
-            spaceBetween={25}
-            rewind={true}
-          >
-            <SwiperSlide>
-              <article className="relative">
-                <Image
-                  src={images[1]}
-                  className=""
-                  alt="Couches with 2 seats"
-                />
-
-                <div className="absolute px-6 py-4 right-8 bottom-8 bg-white/60 backdrop-blur-sm">
-                  <h3 className="text-xl font-medium">Bohauss</h3>
-                  <p className="mb-1 text-sm text-gray">
-                    Luxury big sofa 2-seat
-                  </p>
-
-                  <div className="flex items-center justify-between ">
-                    <span className="block">Rp 17.000.000</span>
-                    <span className="">
-                      <ArrowRightIcon className="w-4 h-4" />
-                    </span>
-                  </div>
-                </div>
-              </article>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <article className="relative">
-                <Image
-                  src={images[0]}
-                  className="w-full"
-                  alt="Couches with 2 seats"
-                />
-
-                <div className="absolute px-6 py-4 right-8 bottom-8 bg-white/60 backdrop-blur-sm">
-                  <h3 className="text-xl font-medium">Bohauss</h3>
-                  <p className="mb-1 text-sm text-gray">
-                    Luxury big sofa 2-seat
-                  </p>
-
-                  <div className="flex items-center justify-between ">
-                    <span className="block">Rp 17.000.000</span>
-                    <span className="">
-                      <ArrowRightIcon className="w-4 h-4" />
-                    </span>
-                  </div>
-                </div>
-              </article>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <article className="relative">
-                <Image
-                  src={images[2]}
-                  className=""
-                  alt="Couches with 2 seats"
-                />
-
-                <div className="absolute px-6 py-4 right-8 bottom-8 bg-white/60 backdrop-blur-sm">
-                  <h3 className="text-xl font-medium">Bohauss</h3>
-                  <p className="mb-1 text-sm text-gray">
-                    Luxury big sofa 2-seat
-                  </p>
-
-                  <div className="flex items-center justify-between ">
-                    <span className="block">Rp 17.000.000</span>
-                    <span className="">
-                      <ArrowRightIcon className="w-4 h-4" />
-                    </span>
-                  </div>
-                </div>
-              </article>
-            </SwiperSlide>
-          </Swiper>
         </div>
+        {/* Slider */}
+        <Swiper
+          className="relative"
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          navigation
+          pagination={{ clickable: true }}
+          slidesPerView={1.85}
+          spaceBetween={25}
+          rewind={true}
+        >
+          <SwiperSlide>
+            <article className="relative">
+              <Image src={images[1]} className="" alt="Couches with 2 seats" />
+
+              <div className="absolute px-6 py-4 right-8 bottom-8 bg-white/60 backdrop-blur-sm">
+                <h3 className="text-xl font-medium">Bohauss</h3>
+                <p className="mb-1 text-sm text-gray">Luxury big sofa 2-seat</p>
+
+                <div className="flex items-center justify-between ">
+                  <span className="block">Rp 17.000.000</span>
+                  <span className="">
+                    <ArrowRightIcon className="w-4 h-4" />
+                  </span>
+                </div>
+              </div>
+            </article>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <article className="relative">
+              <Image
+                src={images[0]}
+                className="w-full"
+                alt="Couches with 2 seats"
+              />
+
+              <div className="absolute px-6 py-4 right-8 bottom-8 bg-white/60 backdrop-blur-sm">
+                <h3 className="text-xl font-medium">Bohauss</h3>
+                <p className="mb-1 text-sm text-gray">Luxury big sofa 2-seat</p>
+
+                <div className="flex items-center justify-between ">
+                  <span className="block">Rp 17.000.000</span>
+                  <span className="">
+                    <ArrowRightIcon className="w-4 h-4" />
+                  </span>
+                </div>
+              </div>
+            </article>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <article className="relative">
+              <Image src={images[2]} className="" alt="Couches with 2 seats" />
+
+              <div className="absolute px-6 py-4 right-8 bottom-8 bg-white/60 backdrop-blur-sm">
+                <h3 className="text-xl font-medium">Bohauss</h3>
+                <p className="mb-1 text-sm text-gray">Luxury big sofa 2-seat</p>
+
+                <div className="flex items-center justify-between ">
+                  <span className="block">Rp 17.000.000</span>
+                  <span className="">
+                    <ArrowRightIcon className="w-4 h-4" />
+                  </span>
+                </div>
+              </div>
+            </article>
+          </SwiperSlide>
+        </Swiper>
       </section>
 
       {/* Features section */}
--- client/components/Layout/Footer.js
+++ client/components/Layout/Footer.js
@@ -12,184 +12,186 @@ import {
 
 const Footer = () => {
   return (
-    <footer className="relative px-6 mt-64 mb-12 overflow-hidden font-second md:px-14">
-      <div className="grid gap-10 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5">
-        {/* Footer logo, description, address and phone */}
-        <div className="text-gray">
-          <Link
-            href="/"
-            className="inline-block mb-4 text-xl font-bold tracking-wide duration-300 text-darkerGray hover:text-main"
-          >
-            Funiro.
-          </Link>
+    <footer className="relative px-6 mt-32 mb-12 overflow-hidden font-second md:px-14">
+      <div className="py-16 border-t-2 border-gray/20">
+        <div className="grid gap-10 pt-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5">
+          {/* Footer logo, description, address and phone */}
+          <div className="text-gray">
+            <Link
+              href="/"
+              className="inline-block mb-4 text-xl font-bold tracking-wide duration-300 text-darkerGray hover:text-main"
+            >
+              Funiro.
+            </Link>
 
-          <p className="max-w-xs mb-4 text-sm">
-            Worldwide furniture store since 2020. We sell over 1000+ branded
-            products on our website
-          </p>
+            <p className="max-w-xs mb-4 text-sm">
+              Worldwide furniture store since 2020. We sell over 1000+ branded
+              products on our website
+            </p>
 
-          <p className="flex items-center gap-2 mb-3 text-xs font-medium">
-            <MapPinIcon className="w-4 h-4" /> Sawojajar Malang, Indonesia
-          </p>
+            <p className="flex items-center gap-2 mb-3 text-xs font-medium">
+              <MapPinIcon className="w-4 h-4" /> Sawojajar Malang, Indonesia
+            </p>
 
-          <p className="flex items-center gap-2 mb-3 text-xs font-medium">
-            <PhoneIcon className="w-4 h-4" /> +6289 456 3455
-          </p>
+            <p className="flex items-center gap-2 mb-3 text-xs font-medium">
+              <PhoneIcon className="w-4 h-4" /> +6289 456 3455
+            </p>
 
-          <Link
-            href={"www.funiro.com"}
-            className="duration-300 hover:text-main"
-          >
-            www.funiro.com
-          </Link>
-        </div>
+            <Link
+              href={"www.funiro.com"}
+              className="duration-300 hover:text-main"
+            >
+              www.funiro.com
+            </Link>
+          </div>
 
-        {/* Menu List */}
-        <div className="grid lg:justify-center ">
-          <h3 className="mb-3 text-lg font-bold footer__title">Menu</h3>
+          {/* Menu List */}
+          <div className="grid lg:justify-center ">
+            <h3 className="mb-3 text-lg font-bold footer__title">Menu</h3>
 
-          <ul className="grid gap-4 text-sm text-gray">
-            <li>
-              <Link
-                href=""
-                className="font-medium duration-300 hover:text-main"
-              >
-                Products
-              </Link>
-            </li>
+            <ul className="grid gap-4 text-sm text-gray">
+              <li>
+                <Link
+                  href=""
+                  className="font-medium duration-300 hover:text-main"
+                >
+                  Products
+                </Link>
+              </li>
 
-            <li>
-              <Link
-                href=""
-                className="font-medium duration-300 hover:text-main"
-              >
-                Rooms
-              </Link>
-            </li>
+              <li>
+                <Link
+                  href=""
+                  className="font-medium duration-300 hover:text-main"
+                >
+                  Rooms
+                </Link>
+              </li>
 
-            <li>
-              <Link
-                href=""
-                className="font-medium duration-300 hover:text-main"
-              >
-                Inspiration
-              </Link>
-            </li>
+              <li>
+                <Link
+                  href=""
+                  className="font-medium duration-300 hover:text-main"
+                >
+                  Inspiration
+                </Link>
+              </li>
 
-            <li>
-              <Link
-                href=""
-                className="font-medium duration-300 hover:text-main"
-              >
-                About Us
-              </Link>
-            </li>
+              <li>
+                <Link
+                  href=""
+                  className="font-medium duration-300 hover:text-main"
+                >
+                  About Us
+                </Link>
+              </li>
 
-            <li>
-              <Link
-                href=""
-                className="font-medium duration-300 hover:text-main"
-              >
-                Terms & Policy
-              </Link>
-            </li>
-          </ul>
-        </div>
+              <li>
+                <Link
+                  href=""
+                  className="font-medium duration-300 hover:text-main"
+                >
+                  Terms & Policy
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-        {/* Account List */}
-        <div>
-          <h3 className="mb-4 text-lg font-bold">Account</h3>
+          {/* Account List */}
+          <div>
+            <h3 className="mb-4 text-lg font-bold">Account</h3>
 
-          <ul className="grid gap-4 text-sm text-gray">
-            <li>
-              <Link
-                href=""
-                className="font-medium duration-300 hover:text-main"
-              >
-                My Account
-              </Link>
-            </li>
+            <ul className="grid gap-4 text-sm text-gray">
+              <li>
+                <Link
+                  href=""
+                  className="font-medium duration-300 hover:text-main"
+                >
+                  My Account
+                </Link>
+              </li>
 
-            <li>
-              <Link
-                href=""
-                className="font-medium duration-300 hover:text-main"
-              >
-                Checkout
-              </Link>
-            </li>
+              <li>
+                <Link
+                  href=""
+                  className="font-medium duration-300 hover:text-main"
+                >
+                  Checkout
+                </Link>
+              </li>
 
-            <li>
-              <Link
-                href=""
-                className="font-medium duration-300 hover:text-main"
-              >
-                My Cart
-              </Link>
-            </li>
+              <li>
+                <Link
+                  href=""
+                  className="font-medium duration-300 hover:text-main"
+                >
+                  My Cart
+                </Link>
+              </li>
 
-            <li>
-              <Link
-                href=""
-                className="font-medium duration-300 hover:text-main"
-              >
-                My Catalog
-              </Link>
-            </li>
-          </ul>
-        </div>
+              <li>
+                <Link
+                  href=""
+                  className="font-medium duration-300 hover:text-main"
+                >
+                  My Catalog
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-        {/* Stay Connected List */}
-        <div>
-          <h3 className="mb-4 text-lg font-bold">Stay Connected</h3>
+          {/* Stay Connected List */}
+          <div>
+            <h3 className="mb-4 text-lg font-bold">Stay Connected</h3>
 
-          <ul className="grid gap-4 text-sm text-gray">
-            <li>
-              <Link
-                href=""
-                className="font-medium duration-300 hover:text-main"
-              >
-                Facebook
-              </Link>
-            </li>
+            <ul className="grid gap-4 text-sm text-gray">
+              <li>
+                <Link
+                  href=""
+                  className="font-medium duration-300 hover:text-main"
+                >
+                  Facebook
+                </Link>
+              </li>
 
-            <li>
-              <Link
-                href=""
-                className="font-medium duration-300 hover:text-main"
-              >
-                Instagram
-              </Link>
-            </li>
+              <li>
+                <Link
+                  href=""
+                  className="font-medium duration-300 hover:text-main"
+                >
+                  Instagram
+                </Link>
+              </li>
 
-            <li>
-              <Link
-                href=""
-                className="font-medium duration-300 hover:text-main"
-              >
-                Twitter
-              </Link>
-            </li>
-          </ul>
-        </div>
+              <li>
+                <Link
+                  href=""
+                  className="font-medium duration-300 hover:text-main"
+                >
+                  Twitter
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-        {/* Stay Updated */}
-        <div>
-          <h3 className="mb-4 text-lg font-bold">Stay updated</h3>
+          {/* Stay Updated */}
+          <div>
+            <h3 className="mb-4 text-lg font-bold">Stay updated</h3>
 
-          <div className="flex items-center mb-4 space-x-1">
-            <input
-              type="text"
-              placeholder="Enter your email"
-              className="block p-1 outline-none bg-gray/10 placeholder:text-sm"
-            />
-
-            <span className="inline-block p-2 cursor-pointer bg-main">
-              <PaperAirplaneIcon
-                className="w-4 h-4 -rotate-[25deg]"
-                color="white"
+            <div className="flex items-center mb-4 space-x-1">
+              <input
+                type="text"
+                placeholder="Enter your email"
+                className="block p-1 outline-none bg-gray/10 placeholder:text-sm"
               />
-            </span>
+
+              <span className="inline-block p-2 cursor-pointer bg-main">
+                <PaperAirplaneIcon
+                  className="w-4 h-4 -rotate-[25deg]"
+                  color="white"
+                />
+              </span>
+            </div>
           </div>
         </div>
       </div>
